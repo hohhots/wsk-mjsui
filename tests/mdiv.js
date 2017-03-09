e7a6e1809d063e24b4f19c261849c6dd20128642
brgd <hohhots@gmail.com>
2017-03-10 01:21:20 +0800

    simple modify

--- tests/mdiv.js
+++ tests/mdiv.js
@@ -17,7 +17,7 @@ define([
     },
 
     'test div height in window': function() {
-      var ih = $(window).innerHeight() - parseInt($('body').css('margin-top')) - parseInt($('body').css('margin-bottom')); 
+      var ih = $(window).innerHeight() - parseInt($('body').css('margin-top')) - parseInt($('body').css('margin-bottom'));
       assert.equal(element.outerWidth(), ih, 'In window mongol div has same height value with window innerHeight.');
     },
 
@@ -25,10 +25,10 @@ define([
       assert.equal(element.css('width'), element.parent('div.mdiv-container').innerWidth()+ 'px');
     }
 	});
-  
+
   registerSuite({
     name: 'mongol div in other div',
-    
+
     'test is div': function() {
       assert.isTrue($('<span class="mongol"></span>').
                     mdiv().hasClass('mongol'), 'Only div html element\'s class \'mongol\' has effect!');
@@ -41,30 +41,30 @@ define([
 
     'test wrapped div of nested divs with mongol class': function() {
       assert.isTrue($('<div class="mongol"><div class="mongol"></div></div>').
-                    find('.mongol').mdiv().hasClass('mongol'), 'Can\'t nest divs with mongol class.');
+                    children('.mongol').mdiv().hasClass('mongol'), 'Can\'t nest divs with mongol class.');
     },
-    
+
     'test mdiv divs wrap mongol mdiv.': function() {
       assert.isTrue($('<div class="mdiv"><div class="mongol"></div></div>').
-                    find('.mongol').mdiv().hasClass('mongol'), 'Can\'t insert mongol div into mdiv div.');
+                    children('.mongol').mdiv().hasClass('mongol'), 'Can\'t insert mongol div into mdiv div.');
     },
 
     'test mongol div wrap mdiv div.': function() {
       assert.isTrue($('<div class="mongol"><div class="mdiv"></div></div>').
                     first().mdiv().hasClass('mongol'), 'mongol div can\'t wrap other div with mdiv class');
     },
-    
+
     'test wrap mongol div correct.': function() {
       assert.isTrue($('<div><div class="mongol"></div></div>').
-                    find('.mongol').mdiv().hasClass('mdiv'), 'mongol div can used in other div which is not mongol or mdiv class div');
+                    children('.mongol').mdiv().hasClass('mdiv'), 'mongol div can used in other div which is not mongol or mdiv class div');
     },
 
     'test mongol div width in other div.': function() {
       var el = $('<div style="height: 100px;width: 200px"><div class="mongol"></div></div>');
-      var h = el.find('.mongol').mdiv().outerWidth();
+      var h = el.children('.mongol').mdiv().outerWidth();
       assert.equal(h, el.innerHeight(), 'mongol div must has same width value with outer container div height value.');
     }
-    
+
 
   });
 })
